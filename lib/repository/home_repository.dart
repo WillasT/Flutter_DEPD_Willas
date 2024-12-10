798d6a0848a9a1fb1a66189235cb94ff19ae91c4
WillasT <wrorrong@student.ciputra.ac.id>
2024-12-10 11:58:22 +0700

    Initial commit

---
+++ lib/repository/home_repository.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_mvvm/data/network/network_api_services.dart';
+import 'package:flutter_mvvm/model/model.dart';
+
+class HomeRepository {
+  final _apiservices = NetworkApiServices();
+
+  Future<List<Province>> fetchProvinceList() async {
+    try {
+      dynamic response = await _apiservices.getApiResponse('/starter/province');
+      List<Province> result = [];
+
+      if (response['rajaongkir']['status']['code'] == 200) {
+        result = (response['rajaongkir']['results'] as List)
+            .map((e) => Province.fromJson(e))
+            .toList();
+      }
+      return result;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<List<City>> fetchCityList(var provId) async {
+    try {
+      dynamic response = await _apiservices.getApiResponse('/starter/city');
+      List<City> result = [];
+
+      if (response['rajaongkir']['status']['code'] == 200) {
+        result = (response['rajaongkir']['results'] as List)
+            .map((e) => City.fromJson(e))
+            .toList();
+      }
+
+      List<City> selectedCities = [];
+      for (var c in result) {
+        if (c.provinceId == provId) {
+          selectedCities.add(c);
+        }
+      }
+
+      return selectedCities;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
---
+++ lib/shared/const.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+class Const {
+  static String baseUrl = 'api.rajaongkir.com';
+  static String apiKey = '6fa2da4f39dea68b8060483de06cdf40';
+}
